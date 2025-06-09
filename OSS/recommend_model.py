62eac1f4999200e1064ba22544c87ef9ccef078a
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-06-09 15:39:51 +0900

    refactor: 엑셀파일 계속 읽기

--- OSS/recommend_model.py
+++ OSS/recommend_model.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
-# 두 모델 결과 파일 불러오기
-xgb_df = pd.read_csv("xgboost_recommendations.csv")        # XGBoost 모델 결과
-surprise_df = pd.read_csv("surprise_recommendations.csv")  # Surprise 모델 결과
+
 
 # ✅ 하이브리드 추천 함수
 def get_hybrid_recommendations(user_id: int, top_n_each: int = 3):
+
+    # 두 모델 결과 파일 불러오기
+    xgb_df = pd.read_csv("xgboost_recommendations.csv")        # XGBoost 모델 결과
+    surprise_df = pd.read_csv("surprise_recommendations.csv")  # Surprise 모델 결과
+    
     # XGBoost 추천 추출
     xgb_top = (
         xgb_df[xgb_df['member_id'] == user_id]
