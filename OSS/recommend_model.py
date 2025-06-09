2681f887567a2cc7da6bbd3719fef09926926289
rnjsgurwns4 <rnjsgurwns4@naver.com>
2025-06-09 13:57:56 +0900

    refactor: 파일 이름 수정

--- OSS/recommend_model.py
+++ OSS/recommend_model.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # 두 모델 결과 파일 불러오기
 xgb_df = pd.read_csv("xgboost_recommendations.csv")        # XGBoost 모델 결과
-surprise_df = pd.read_csv("surprise_recommendations.csv")  # Surprise 모델 결과
+collaborative_df = pd.read_csv("collaborative_recommendations.csv")  # Surprise 모델 결과
 
 # ✅ 하이브리드 추천 함수
 def get_hybrid_recommendations(user_id: int, top_n_each: int = 3):
@@ -15,11 +15,11 @@ def get_hybrid_recommendations(user_id: int, top_n_each: int = 3):
     )
 
     # Surprise 추천 추출
-    surprise_top = (
-        surprise_df[surprise_df['member_id'] == user_id]
+    collaborative_top = (
+        collaborative_df[collaborative_df['member_id'] == user_id]
         .sort_values(by='score', ascending=False)
         .head(top_n_each)['content_detail_id']
         .tolist()
     )
 
-    return xgb_top + surprise_top  # 앞 3개는 xgb, 뒤 3개는 surprise
+    return xgb_top + collaborative_top  # 앞 3개는 xgb, 뒤 3개는 surprise
